bc62a8dfa1df5c46090bb690d313a82e2356b555
SuperEVO <zhang740@vmzclub.com>
2017-08-23 16:52:27 +0800

    add tests.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { default as Calendar } from './Calendar';
+export { default as DatePicker } from './DatePicker';
+
+import zhCN from './locale/zh_CN';
+import enUS from './locale/en_US';
+const Locale = { zhCN, enUS };
+
+export { Locale };
