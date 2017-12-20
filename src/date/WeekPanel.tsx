d74587a020ed3e11c8773358c246cbc5496075b5
Han <me@hankewins.com>
2017-12-20 12:42:52 +0800

    fixed: WeekPanel locale (#3)

--- src/date/WeekPanel.tsx
+++ src/date/WeekPanel.tsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 
 export default class WeekPanel extends React.PureComponent<{}, {}> {
   render() {
+    const { week } = this.props;
     return (
       <div className="week-panel">
-        <div className="cell cell-grey">日</div>
-        <div className="cell">一</div>
-        <div className="cell">二</div>
-        <div className="cell">三</div>
-        <div className="cell">四</div>
-        <div className="cell">五</div>
-        <div className="cell cell-grey">六</div>
+        <div className="cell cell-grey">{week[0]}</div>
+        <div className="cell">{week[1]}</div>
+        <div className="cell">{week[2]}</div>
+        <div className="cell">{week[3]}</div>
+        <div className="cell">{week[4]}</div>
+        <div className="cell">{week[5]}</div>
+        <div className="cell cell-grey">{week[6]}</div>
       </div>
     );
   }
